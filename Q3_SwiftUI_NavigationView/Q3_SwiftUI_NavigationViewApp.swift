40c3e4d4ea1f7680f03def8cbd8886d38c877222
k-yamaki <k.yamaki@hiroshimamm.com>
2021-08-19 14:03:57 +0900

    Initial Commit

---
+++ Q3_SwiftUI_NavigationView/Q3_SwiftUI_NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Q3_SwiftUI_NavigationViewApp.swift
+//  Q3_SwiftUI_NavigationView
+//
+//  Created by keiji yamaki on 2021/08/19.
+//
+
+import SwiftUI
+
+@main
+struct Q3_SwiftUI_NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
